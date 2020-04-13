c4cc63eb032d8d6caf3b50b3caa3c3ca70d3ce16
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-13 23:32:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,3 @@
 def square_array(array)
-count = 0
-new_array = []
-while count  < array.length do 
- new_array.push (array[count] * array[count])  
-count +=1
-end 
-return new_array
-end 
-square_array(array)
-
+  # your code here
+end
